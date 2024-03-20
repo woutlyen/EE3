f03ac9ee22f0f2bbd116e9fa242ca3f0fc84eb6b
Wout Lyen <woutlyen@gmail.com>
2024-03-20 16:45:20 +0100

    Added code for the SD card in main.c

--- components/speaker/speaker.c
+++ components/speaker/speaker.c
@@ -13,14 +13,26 @@ void speaker_init(const char *T, size_t sample_rate, i2s_port_t PORT){
     TAG = T;
     PORT_TX = PORT;
 
+/*
+    i2s_config_t i2s_tx_config = {
+        .mode = I2S_MODE_MASTER | I2S_MODE_TX,
+        .sample_rate = sample_rate,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+        .channel_format = I2S_CHANNEL_STEREO,
+        .communication_format = I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB,
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+        .dma_buf_count = 4,
+        .dma_buf_len = 1024,
+    };*/
+
     i2s_config_t i2s_tx_config = {
         .mode = I2S_MODE_MASTER | I2S_MODE_TX,
         .sample_rate = sample_rate,
         .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
         .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-        .communication_format = I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB,
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = 4,
+        .dma_buf_count = 8,
         .dma_buf_len = 1024,
     };
 
--- main/main.c
+++ main/main.c
@@ -38,6 +38,24 @@
 #include "config.h"
 #include "WavData.h"
 
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+#include "driver/sdspi_host.h"
+#define MOUNT_POINT "/sdcard"
+#define AUDIO_BUFFER 1024
+
+// Pin assignments can be set in menuconfig, see "SD SPI Example Configuration" menu.
+// You can also change the pin assignments here by changing the following 4 lines.
+#define PIN_NUM_MISO  GPIO_NUM_37
+#define PIN_NUM_MOSI  GPIO_NUM_35
+#define PIN_NUM_CLK   GPIO_NUM_36
+#define PIN_NUM_CS    GPIO_NUM_20 
+
+#define EXAMPLE_MAX_CHAR_SIZE    64
+
 esp_mqtt_client_handle_t event_client;
 
 static const char *TAG = "audio_recorder";
@@ -51,6 +69,81 @@ void configure_led(void)
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
     gpio_set_direction(BLINK_GPIO2, GPIO_MODE_OUTPUT);
+}
+
+
+static esp_err_t s_example_write_file(const char *path, char *data)
+{
+    ESP_LOGI(TAG, "Opening file %s", path);
+    FILE *f = fopen(path, "w");
+    if (f == NULL) {
+        ESP_LOGE(TAG, "Failed to open file for writing");
+        return ESP_FAIL;
+    }
+    fprintf(f, data);
+    fclose(f);
+    ESP_LOGI(TAG, "File written");
+
+    return ESP_OK;
+}
+
+static esp_err_t s_example_read_file(const char *path)
+{
+    ESP_LOGI(TAG, "Reading file %s", path);
+    FILE *f = fopen(path, "r");
+    if (f == NULL) {
+        ESP_LOGE(TAG, "Failed to open file for reading");
+        return ESP_FAIL;
+    }
+    char line[EXAMPLE_MAX_CHAR_SIZE];
+    fgets(line, sizeof(line), f);
+    fclose(f);
+
+    // strip newline
+    char *pos = strchr(line, '\n');
+    if (pos) {
+        *pos = '\0';
+    }
+    ESP_LOGI(TAG, "Read from file: '%s'", line);
+
+    return ESP_OK;
+}
+
+static esp_err_t play_wav(const char *path)
+{
+  FILE *fh = fopen(path, "rb");
+  if (fh == NULL)
+  {
+    ESP_LOGE(TAG, "Failed to open file");
+    return ESP_ERR_INVALID_ARG;
+  }
+
+  // skip the header...
+  fseek(fh, 44, SEEK_SET);
+
+  // create a writer buffer
+  uint8_t *buf = calloc(AUDIO_BUFFER, sizeof(int8_t));
+  size_t bytes_read = 0;
+  size_t bytes_written = 0;
+
+  bytes_read = fread(buf, sizeof(int8_t), AUDIO_BUFFER, fh);
+
+  speaker_init(TAG, 48000, I2S_NUM_TX);
+
+  while (bytes_read > 0)
+  {
+    speaker_play(buf, AUDIO_BUFFER);
+    // write the buffer to the i2s
+    //i2s_channel_write(tx_handle, buf, bytes_read * sizeof(int16_t), &bytes_written, portMAX_DELAY);
+    bytes_read = fread(buf, sizeof(int8_t), AUDIO_BUFFER, fh);
+    //ESP_LOGV(TAG, "Bytes read: %d", bytes_read);
+  }
+
+  //i2s_channel_disable(tx_handle);
+  speaker_stop();
+  free(buf);
+  fclose(fh);
+  return ESP_OK;
 }
 
 
@@ -76,10 +169,186 @@ void app_main(void)
     
 
 
+    esp_err_t ret;
+
+    // Options for mounting the filesystem.
+    // If format_if_mount_failed is set to true, SD card will be partitioned and
+    // formatted in case when mounting fails.
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+#ifdef CONFIG_EXAMPLE_FORMAT_IF_MOUNT_FAILED
+        .format_if_mount_failed = true,
+#else
+        .format_if_mount_failed = false,
+#endif // EXAMPLE_FORMAT_IF_MOUNT_FAILED
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+    sdmmc_card_t *card;
+    const char mount_point[] = MOUNT_POINT;
+    ESP_LOGI(TAG, "Initializing SD card");
+
+    // Use settings defined above to initialize SD card and mount FAT filesystem.
+    // Note: esp_vfs_fat_sdmmc/sdspi_mount is all-in-one convenience functions.
+    // Please check its source code and implement error recovery when developing
+    // production applications.
+    ESP_LOGI(TAG, "Using SPI peripheral");
+
+    // By default, SD card frequency is initialized to SDMMC_FREQ_DEFAULT (20MHz)
+    // For setting a specific frequency, use host.max_freq_khz (range 400kHz - 20MHz for SDSPI)
+    // Example: for fixed frequency of 10MHz, use host.max_freq_khz = 10000;
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+
+    spi_bus_config_t bus_cfg = {
+        .mosi_io_num = PIN_NUM_MOSI,
+        .miso_io_num = PIN_NUM_MISO,
+        .sclk_io_num = PIN_NUM_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 4000,
+    };
+    ret = spi_bus_initialize(host.slot, &bus_cfg, SDSPI_DEFAULT_DMA);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize bus.");
+        return;
+    }
+
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_config.gpio_cs = PIN_NUM_CS;
+    slot_config.host_id = host.slot;
+
+    ESP_LOGI(TAG, "Mounting filesystem");
+    ret = esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &card);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount filesystem. "
+                     "If you want the card to be formatted, set the CONFIG_EXAMPLE_FORMAT_IF_MOUNT_FAILED menuconfig option.");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+                     "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+        }
+        return;
+    }
+    ESP_LOGI(TAG, "Filesystem mounted");
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+
+    // Use POSIX and C standard library functions to work with files.
+
+    // First create a file.
+    const char *file_hello = MOUNT_POINT"/hello.txt";
+    char data[EXAMPLE_MAX_CHAR_SIZE];
+    snprintf(data, EXAMPLE_MAX_CHAR_SIZE, "%s %s!\n", "Hello", card->cid.name);
+    ret = s_example_write_file(file_hello, data);
+    if (ret != ESP_OK) {
+        return;
+    }
+
+    const char *file_foo = MOUNT_POINT"/foo.txt";
+
+    // Check if destination file exists before renaming
+    struct stat st;
+    if (stat(file_foo, &st) == 0) {
+        // Delete it if it exists
+        unlink(file_foo);
+    }
+
+    // Rename original file
+    ESP_LOGI(TAG, "Renaming file %s to %s", file_hello, file_foo);
+    if (rename(file_hello, file_foo) != 0) {
+        ESP_LOGE(TAG, "Rename failed");
+        return;
+    }
+
+    ret = s_example_read_file(file_foo);
+    if (ret != ESP_OK) {
+        return;
+    }
+
+    // Format FATFS
+#ifdef CONFIG_EXAMPLE_FORMAT_SD_CARD
+    ret = esp_vfs_fat_sdcard_format(mount_point, card);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to format FATFS (%s)", esp_err_to_name(ret));
+        return;
+    }
+
+    if (stat(file_foo, &st) == 0) {
+        ESP_LOGI(TAG, "file still exists");
+        return;
+    } else {
+        ESP_LOGI(TAG, "file doesnt exist, format done");
+    }
+#endif // CONFIG_EXAMPLE_FORMAT_SD_CARD
+
+    const char *file_nihao = MOUNT_POINT"/nihao.txt";
+    memset(data, 0, EXAMPLE_MAX_CHAR_SIZE);
+    snprintf(data, EXAMPLE_MAX_CHAR_SIZE, "%s %s!\n", "Nihao", card->cid.name);
+    ret = s_example_write_file(file_nihao, data);
+    if (ret != ESP_OK) {
+        return;
+    }
+
+    //Open file for reading
+    ret = s_example_read_file(file_nihao);
+    if (ret != ESP_OK) {
+        return;
+    }
+
+    const char *tour = MOUNT_POINT"/tour.wav";
+    //ret = play_wav(tour);
+    //if (ret != ESP_OK) {
+    //    return;
+    //}
+
+    const char *tour2 = MOUNT_POINT"/promised.wav";
+    ret = play_wav(tour2);
+    if (ret != ESP_OK) {
+        return;
+    }
+
+    const char *tour3 = MOUNT_POINT"/dansen.wav";
+    ret = play_wav(tour3);
+    if (ret != ESP_OK) {
+        return;
+    }
+
+    const char *tour4 = MOUNT_POINT"/turbo.wav";
+    ret = play_wav(tour4);
+    if (ret != ESP_OK) {
+        return;
+    }
+
+    const char *tour5 = MOUNT_POINT"/gold.wav";
+    ret = play_wav(tour5);
+    if (ret != ESP_OK) {
+        return;
+    }
+
+    const char *tour6 = MOUNT_POINT"/trip.wav";
+
+    ret = play_wav(tour6);
+    if (ret != ESP_OK) {
+        return;
+    }
+
+
+    // All done, unmount partition and disable SPI peripheral
+    esp_vfs_fat_sdcard_unmount(mount_point, card);
+    ESP_LOGI(TAG, "Card unmounted");
+
+    //deinitialize the bus after all devices are removed
+    spi_bus_free(host.slot);
     
 }
 
 static void *AI_thread(void * arg){
+
+    return NULL;
+
     while(true){
     
     speaker_init(TAG, 44100, I2S_NUM_TX);
@@ -124,6 +393,9 @@ static void *AI_thread(void * arg){
 }
 
 static void *MQTT_LIGHT_TEMP_thread(void * arg){
+
+    return NULL;
+    
     init_rgb_light();
     init_dimmable_light();
     init_normal_light();
